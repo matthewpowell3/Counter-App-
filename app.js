18e94bcfa8a45a5035e65669dd34a6a84bc615c7
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-14 12:07:13 -0500

    1 file modified

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-// Variable
+// Button Variables
 const increaseValue = document.getElementsByClassName('increase');
 const resetValue = document.getElementsByClassName('reset');
 const decreaseValue = document.getElementsByClassName('decrease');
+
+// Number Values
+const value = document.getElementById('value');
+
+
+// 
