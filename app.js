e16d2cdf3191a3035562b9f9b5240c8ff198a6af
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-14 12:05:39 -0500

    1 file modified

---
+++ app.js
@@ -0,0 +1,4 @@
+// Variable
+const increaseValue = document.getElementsByClassName('increase');
+const resetValue = document.getElementsByClassName('reset');
+const decreaseValue = document.getElementsByClassName('decrease');
